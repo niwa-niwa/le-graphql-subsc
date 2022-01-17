7b8ffee3b5657ab25d6fd6a1bb60ed9af9b96a20
niwa-niwa <urban.hang274@gmail.com>
2022-01-18 00:22:29 +0900

    implemented database with object

---
+++ db.ts
@@ -0,0 +1,21 @@
+const posts = [
+  {
+    id:'1',
+    title:'こころ',
+    author: '夏目漱石'
+  },
+  {
+    id:'2',
+    title:'舞姫',
+    author:'森'
+  },
+  {
+    id:'3',
+    title:'羅生門',
+    author:'芥川龍之介'
+  }
+]
+
+const db = { posts, }
+
+module.exports = db;
--- schema.ts
+++ schema.ts
@@ -12,9 +12,36 @@ type Mutation {
   updatePost(id: ID!, data: UpdatePostInput!): Post!
 }
 
-type subscription {
+type Subscription {
   post: PostSubscriptionPayload!
 }
 
+input CreatePostInput {
+  title: String!
+  author: String!
+}
 
+input UpdatePostInput {
+  title: String
+  author: String!
+}
+
+type Post {
+  id: ID!
+  title: String!
+  author: String!
+}
+
+enum MutationType {
+  CREATED
+  UPDATED
+  DELETED
+}
+
+type PostSubscriptionPayload {
+  mutation: MutationType!
+  data: Post!
+}
 `
+
+module.exports = typeDefs
